1bcbd3d25ba73ea8e5d5f7f49b9828fdd01a9b5d
joamag <joamag@hive.pt>
2018-03-13 10:39:29 +0000

    new code

--- src/main/java/hive/examples/java/Main.java
+++ src/main/java/hive/examples/java/Main.java
@@ -1,3 +1,6 @@
+package hive.examples.java;
+
+import hive.examples.java.http.HttpExample;
 import org.apache.http.HttpEntity;
 import org.apache.http.client.methods.CloseableHttpResponse;
 import org.apache.http.client.methods.HttpGet;
@@ -11,39 +14,9 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    /**
-     * The URL that is going to be used for the example HTTP call.
-     */
-    private static String EXAMPLE_HTTP_URL = "https://libs.stage.hive.pt/";
-
-    private static void exampleHttp() throws IOException {
-        exampleHttp(EXAMPLE_HTTP_URL);
-    }
-
-    private static void exampleHttp(String url) throws IOException {
-        CloseableHttpResponse response;
-        CloseableHttpClient httpclient = HttpClients.createDefault();
-        HttpGet httpget = new HttpGet(url);
-        response = httpclient.execute(httpget);
-        try {
-            HttpEntity entity = response.getEntity();
-            InputStream inputStream = entity.getContent();
-            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-            while (true) {
-                String line = bufferedReader.readLine();
-                if (line == null) {
-                    break;
-                }
-                System.out.println(line);
-            }
-        } finally {
-            response.close();
-        }
-    }
-
     public static void main(String[] args) {
         try {
-            exampleHttp();
+            HttpExample.run();
         } catch (IOException exception) {
             exception.printStackTrace();
         }
--- src/main/java/hive/examples/java/http/HttpExample.java
+++ src/main/java/hive/examples/java/http/HttpExample.java
@@ -1,4 +1,49 @@
 package hive.examples.java.http;
 
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.CloseableHttpResponse;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.impl.client.CloseableHttpClient;
+import org.apache.http.impl.client.HttpClients;
+
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+
 public class HttpExample {
+
+    /**
+     * The URL that is going to be used for the example HTTP call.
+     */
+    private static String EXAMPLE_HTTP_URL = "https://libs.stage.hive.pt/";
+
+    public static void run() throws IOException {
+        exampleHttp();
+    }
+
+    private static void exampleHttp() throws IOException {
+        exampleHttp(EXAMPLE_HTTP_URL);
+    }
+
+    private static void exampleHttp(String url) throws IOException {
+        CloseableHttpResponse response;
+        CloseableHttpClient httpclient = HttpClients.createDefault();
+        HttpGet httpget = new HttpGet(url);
+        response = httpclient.execute(httpget);
+        try {
+            HttpEntity entity = response.getEntity();
+            InputStream inputStream = entity.getContent();
+            BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
+            while (true) {
+                String line = bufferedReader.readLine();
+                if (line == null) {
+                    break;
+                }
+                System.out.println(line);
+            }
+        } finally {
+            response.close();
+        }
+    }
 }
